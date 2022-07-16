24af32c97375b13ece10a6c9c1d185219a9c6ea6
carlatakagi <carlatakagi@gmail.com>
2022-07-15 20:43:39 -0400

    feat: add context

---
+++ src/context/EquitiesProvider.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import EquitiesContext from './EquitiesContext';
+
+function EquitiesProvider({children}) {
+
+  return (
+    <EquitiesContext.Provider value={ {children} }>
+      {children}
+    </EquitiesContext.Provider>);
+}
+
+EquitiesProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default EquitiesProvider; 
