43828b4b07f53575667120c5199d0363602249e8
carlatakagi <carlatakagi@gmail.com>
2022-07-15 22:13:53 -0400

    feat: add email and password validation, save user on localstorage

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,21 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function Login() {
+  const navigate = useNavigate();
+  const MIN_CHARACTERS = 6;
+
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [buttonDisabled, setButtonDisabled] = useState(true);
+
+  useEffect(() => {
+    if(email.includes('@') && email.includes('.com') && password.length >= MIN_CHARACTERS) {
+      setButtonDisabled(false);
+    } else {
+    setButtonDisabled(true);
+    }
+  }, [email, password]);
 
   const handleEmailChange = (e) => {
     setEmail(e.target.value);
@@ -10,6 +23,18 @@ function Login() {
 
   const handlePasswordChange = (e) => {
     setPassword(e.target.value);
+  }
+
+  // falta salvar data e hora de acesso
+  const handleClick = (e) => {
+    e.preventDefault();
+    console.log('clicou');
+
+    const userEmail = {email: email};
+    const userPassword = {password: password};
+
+    localStorage.setItem('email', JSON.stringify(userEmail));
+    navigate('/equities');
   }
 
   return (
@@ -38,8 +63,9 @@ function Login() {
         </label>
 
         <button
-          type="submit"
-          onClick={console.log('clicou')}
+          disabled={buttonDisabled}
+          onClick={handleClick}
+          type="submit"  
         >
           Sign in
         </button>
