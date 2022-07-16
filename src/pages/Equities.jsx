271b80c00892c94ee233f7d4a57c441d81a97aa3
carlatakagi <carlatakagi@gmail.com>
2022-07-15 23:07:14 -0400

    feat: add header component to pages

--- src/pages/Equities.jsx
+++ src/pages/Equities.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import Header from '../components/Header';
 
 function Equities () {
+  const navigate = useNavigate();
+
+  const handleClick = (e) => {
+    e.preventDefault();
+
+    navigate('/depositandwithdraw');
+  };
+
   return (
     <div>
+      <Header /> 
       pagina de acoes
+
+      <button
+        type="submit"
+        disabled={false}
+        onClick={handleClick}
+      >
+        Deposit/ Withdraw
+      </button>
     </div>
   )
 }
