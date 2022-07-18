9507096bef7348201e3f60bd1b5a39abc4d6eeb2
carlatakagi <carlatakagi@gmail.com>
2022-07-17 23:19:58 -0400

    fix: changing file

--- src/pages/PurchaseAndSale/index.jsx
+++ src/pages/PurchaseAndSale/index.jsx
@@ -1,18 +1,48 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import Header from '../../components/Header/index';
-import TableMyEquities from '../../components/TableMyEquities';
+import TablePurchaseAndSale from '../../components/TablePurchaseAndSale';
 import './styles.css';
 
 function PurchaseAndSale () {
+  const navigate = useNavigate();
+
+  const handleClickReturn = (e) => {
+    e.preventDefault();
+    navigate('/equities');
+  }
+
+  const handleClickConfirm = (e) => {
+    e.preventDefault();
+    console.log('clicou no confirmar');
+    window.alert("Seu saldo será atualizado em até 24h.")
+  }
+
   return (
     <div>
       <Header />
       
       <div className="title-purchaseandsale">
-        <h1>pagina de compra e venda de acoes</h1>
+        <h1>Negociações</h1>
       </div>
 
-      <TableMyEquities />
+      <TablePurchaseAndSale />
+
+      <div className="button-purchaseandsale">
+        <button
+          onClick={handleClickReturn}
+          type="submit"  
+        >
+          Voltar
+        </button>
+
+          <button
+            onClick={handleClickConfirm}
+            type="button"  
+          >
+            Confirmar
+          </button>
+      </div>
     </div>
   )
 }
