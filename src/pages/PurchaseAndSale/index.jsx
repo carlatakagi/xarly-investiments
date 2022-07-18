a690a2a91aed709346eccde6cc2abc7eec80bf17
carlatakagi <carlatakagi@gmail.com>
2022-07-17 21:03:11 -0400

    fix: changing file

--- src/pages/PurchaseAndSale/index.jsx
+++ src/pages/PurchaseAndSale/index.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import Header from '../../components/Header/index';
+import TableMyEquities from '../../components/TableMyEquities';
+import './styles.css';
 
 function PurchaseAndSale () {
   return (
     <div>
       <Header />
       
-      pagina de compra e venda de acoes
+      <div className="title-purchaseandsale">
+        <h1>pagina de compra e venda de acoes</h1>
+      </div>
+
+      <TableMyEquities />
     </div>
   )
 }
