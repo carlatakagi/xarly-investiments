0d53a4a32cef12ae421b3c3fad19d476a110b0d0
carlatakagi <carlatakagi@gmail.com>
2022-07-17 23:20:42 -0400

    feat: add new file

---
+++ src/components/TablePurchaseAndSale/index.jsx
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import './styles.css';
+
+function TablePurchaseAndSale () {
+
+  return(
+    <div className="table">
+      <table>
+        <thead>
+          <tr>
+            <th>Minhas ações</th>
+          </tr>
+          
+          <tr>
+            <th>Ação</th>
+            <th>Quantidade</th>
+            <th>Valor (R$)</th>
+            <th>Negociar</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>WEGE3</td>
+            <td>1</td>
+            <td>26.55</td>
+            <td>
+              <Link to={'/purchaseandsale'}>Comprar/ Vender</Link>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default TablePurchaseAndSale;
