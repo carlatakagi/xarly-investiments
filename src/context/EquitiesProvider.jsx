ae00df1d4d7c1e5a80f2b32ad965a8ccb0b98caf
carlatakagi <carlatakagi@gmail.com>
2022-07-18 22:09:18 -0400

    fix: changing files

--- src/context/EquitiesProvider.jsx
+++ src/context/EquitiesProvider.jsx
@@ -1,13 +1,47 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import EquitiesContext from './EquitiesContext';
+import { fetchApi } from '../services/fetchApi';
 
 function EquitiesProvider({children}) {
+  const [allEquities, setAllEquities] = useState([]);
+  const [saleEquities, setSaleEquities] = useState([]);
+  const [myEquities, setMyEquities] = useState([]);
+
+  const getEquitiesFromApi = () => {
+    fetchApi()
+      .then((response) => {
+        setAllEquities(response);
+        setSaleEquities(response);
+      });
+  };
+
+  useEffect(() => { getEquitiesFromApi(); }, []);
+
+  const value = {
+    allEquities,
+    setAllEquities,
+    saleEquities,
+    setSaleEquities,
+    myEquities,
+    setMyEquities
+  }
+
   return (
-    <EquitiesContext.Provider value={ {children} }>
+    <EquitiesContext.Provider value={ value }>
       {children}
     </EquitiesContext.Provider>);
 }
+
+export const useEquitiesContext = () => {
+  const context = useContext(EquitiesContext);
+
+  if (context === 'undefined') {
+    throw new Error('useEquitiesContext não existe');
+  }
+
+  return context;
+};
 
 EquitiesProvider.propTypes = {
   children: PropTypes.node.isRequired,
