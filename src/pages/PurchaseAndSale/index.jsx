8c5e177424a9e55b4871f7e16660b81ed3ca9dea
carlatakagi <carlatakagi@gmail.com>
2022-07-19 09:19:18 -0400

    fix: changing file - add button

--- src/pages/PurchaseAndSale/index.jsx
+++ src/pages/PurchaseAndSale/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Header from '../../components/Header/index';
 import TablePurchaseAndSale from '../../components/TablePurchaseAndSale';
@@ -6,6 +6,11 @@ import './styles.css';
 
 function PurchaseAndSale () {
   const navigate = useNavigate();
+  const [value, setValue] = useState(0);
+
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  }
 
   const handleClickReturn = (e) => {
     e.preventDefault();
@@ -15,7 +20,7 @@ function PurchaseAndSale () {
   const handleClickConfirm = (e) => {
     e.preventDefault();
     console.log('clicou no confirmar');
-    window.alert("Seu saldo será atualizado em até 24h.")
+    window.alert("Sua página de ações será atualizada em até 1h.")
   }
 
   return (
@@ -27,6 +32,47 @@ function PurchaseAndSale () {
       </div>
 
       <TablePurchaseAndSale />
+
+      <div className="div-form-purchaseandsale">
+        <form className="form-purchase-sale">
+          <div className="input-sale">
+
+            <button
+              type="button"
+              onClick={console.log('clicou no botao comprar valor')}
+            >
+              Comprar
+            </button>
+            <label htmlFor="valuesale">
+              <input
+                name="value"
+                placeholder="Informe o valor"
+                onChange={e => handleChange(e)}
+                type="number"
+              />
+            </label>
+          </div>
+
+          <div className="input-purchase">
+            <button
+              type="button"
+              onClick={console.log('clicou no botao vender valor')}
+            >
+              Vender
+            </button>
+
+            <label htmlFor="valuepurchase">
+              <input
+                name="value"
+                placeholder="Informe o valor"
+                onChange={e => handleChange(e)}
+                type="number"
+              />
+            </label>
+
+          </div>
+        </form>
+      </div>
 
       <div className="button-purchaseandsale">
         <button
