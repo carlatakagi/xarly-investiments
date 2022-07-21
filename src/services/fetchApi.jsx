5a607cf49e76ece2ac835c40ac6b56bfa6005e51
carlatakagi <carlatakagi@gmail.com>
2022-07-20 23:33:02 -0400

    fix: changing file

--- src/services/fetchApi.jsx
+++ src/services/fetchApi.jsx
@@ -1,8 +1,7 @@
-const API = 'https://api-ficticia-de-acoes.herokuapp.com/';
+const baseURL = 'https://api-ficticia-de-acoes.herokuapp.com';
 
-export const fetchApi = async () => {
-  const request = await fetch(API);
+export const fetchEquitiesFromApi = async (codCliente) => {
+  const request = await fetch(`${baseURL}/cliente/ativos/${codCliente}`);
   const result = request.json();
-
   return result;
 }
