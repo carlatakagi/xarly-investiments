ba029f0a12cbe4a90dc626a4e97049af6c63ce64
carlatakagi <carlatakagi@gmail.com>
2022-07-20 23:32:52 -0400

    fix: changing file

--- src/pages/PurchaseAndSale/index.jsx
+++ src/pages/PurchaseAndSale/index.jsx
@@ -1,23 +1,16 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 import Header from '../../components/Header/index';
 import TablePurchaseAndSale from '../../components/TablePurchaseAndSale';
 import './styles.css';
 
 function PurchaseAndSale () {
   const navigate = useNavigate();
-  const [value, setValue] = useState(0);
+  const {state: {isSelling}} = useLocation();
 
   const handleChange = (e) => {
     setValue(e.target.value);
   }
-
-  /* const handleChangeColor = (e) => {
-    const purchaseBtn = document.querySelector('.purchase-btn');
-    const saleBtn = document.querySelector('.sale-btn');
-
-    if(purchaseBtn.style.backgroundColor === #0062ff)
-  } */
 
   const handleClickReturn = (e) => {
     e.preventDefault();
@@ -26,7 +19,6 @@ function PurchaseAndSale () {
 
   const handleClickConfirm = (e) => {
     e.preventDefault();
-    console.log('clicou no confirmar');
     window.alert("Sua página de ações será atualizada em até 1h.")
   }
 
@@ -44,13 +36,29 @@ function PurchaseAndSale () {
         <form className="form-purchase-sale">
           <div className="input-sale">
 
-            <button
-              className="purchase-btn"
-              type="button"
-              onClick={console.log('clicou no botao comprar valor')}
+          {isSelling ? 
+            <div className="input-purchase">
+              <span
+              >
+                Vender
+              </span>
+
+              <label htmlFor="valuepurchase">
+                <input
+                  name="value"
+                  placeholder="Informe o valor"
+                  onChange={e => handleChange(e)}
+                  type="number"
+                />
+              </label>
+
+            </div>
+            :
+            <div className="purchase-btn">
+            <span
             >
               Comprar
-            </button>
+            </span>
             <label htmlFor="valuesale">
               <input
                 name="value"
@@ -60,25 +68,7 @@ function PurchaseAndSale () {
               />
             </label>
           </div>
-
-          <div className="input-purchase">
-            <button
-              className="sale-btn"
-              type="button"
-              onClick={console.log('clicou no botao vender valor')}
-            >
-              Vender
-            </button>
-
-            <label htmlFor="valuepurchase">
-              <input
-                name="value"
-                placeholder="Informe o valor"
-                onChange={e => handleChange(e)}
-                type="number"
-              />
-            </label>
-
+        }  
           </div>
         </form>
       </div>
