dadeb0ad495c2cc95c659b376e359b1a0fe8d165
carlatakagi <carlatakagi@gmail.com>
2022-07-22 22:05:39 -0400

    feat: add fetch api

--- src/services/fetchApi.jsx
+++ src/services/fetchApi.jsx
@@ -1,7 +1,50 @@
-const baseURL = 'https://api-ficticia-de-acoes.herokuapp.com';
+const baseURL = 'https://equities-api.herokuapp.com/';
 
-export const fetchEquitiesFromApi = async (codCliente) => {
-  const request = await fetch(`${baseURL}/cliente/ativos/${codCliente}`);
+export const fetchEquitiesByClientId = async (codCliente) => {
+  const request = await fetch(`${baseURL}/client/ativos/${codCliente}`);
   const result = request.json();
+
+  return result;
+}
+
+export const fetchEquityById = async (codAtivos) => {
+  const request = await fetch(`${baseURL}/assets/ativos/${codAtivos}`);
+  const result = request.json();
+
+  return result;
+}
+
+export const fetchBuyEquity = async () => {
+  const request = await fetch(`${baseURL}/assets/investimentos/comprar`);
+  const result = request.json();
+
+  return result;
+}
+
+export const fetchSellEquity = async () => {
+  const request = await fetch(`${baseURL}/assets/investimentos/vender`);
+  const result = request.json();
+
+  return result;
+}
+
+export const fetchMakeDeposit = async () => {
+  const request = await fetch(`${baseURL}/conta/deposito`);
+  const result = request.json();
+
+  return result;
+}
+
+export const fetchMakeWithdraw = async () => {
+  const request = await fetch(`${baseURL}/conta/saque`);
+  const result = request.json();
+
+  return result;
+}
+
+export const fetchClientAccount = async (codCliente) => {
+  const request = await fetch(`${baseURL}/conta/${codCliente}`);
+  const result = request.json();
+
   return result;
 }
