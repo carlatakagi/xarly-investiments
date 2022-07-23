e7bbf579e1e4cd74259f0b3e059edd995a755771
carlatakagi <carlatakagi@gmail.com>
2022-07-23 11:25:01 -0400

    feat: changing files

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  test: {
+    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    globals: true,
+    environment: 'jsdom',
+  },
 })
