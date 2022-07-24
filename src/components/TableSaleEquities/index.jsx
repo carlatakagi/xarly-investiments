7301178d766d18ef941eeee15e0767c3ebe548b7
carlatakagi <carlatakagi@gmail.com>
2022-07-24 16:10:53 -0400

    feat: changing files

--- src/components/TableSaleEquities/index.jsx
+++ src/components/TableSaleEquities/index.jsx
@@ -1,63 +1,60 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import EquitiesContext from '../../context/EquitiesContext';
-import { useEquitiesContext } from '../../context/EquitiesProvider';
+import { useEquitiesContext } from '../../shared/providers/EquitiesProvider';
 import './styles.css';
 
 
 function TableSaleEquities () {
   const navigate = useNavigate();
 
-  const {saleEquities} = useEquitiesContext(EquitiesContext);
+  const { saleEquities } = useEquitiesContext();
 
-  const handleClick = (e, codAtivo) => {
+  const handleClick = (e, equityId) => {
     e.preventDefault();
 
     const isSelling = e.target.innerText === 'V';
-    navigate(`/purchaseandsale/${codAtivo}`, {state: {isSelling}});
+
+    navigate(`/purchaseandsale/${equityId}`, {state: { isSelling }});
   }
 
   return(
     <div className="table-sale-equities">
       {
-            saleEquities.length > 0
-            ?
-            <table>
-              <thead>
-                <tr>
-                  <th>Disponíveis</th>
-                </tr>
+        saleEquities.length > 0 ?
+          <table>
+            <thead>
+              <tr>
+                <th>Disponíveis</th>
+              </tr>
 
-                <tr>
-                  <th>Ação</th>
-                  <th>Quantidade</th>
-                  <th>Valor (R$)</th>
-                  <th>Negociar</th>
-                </tr>
-              </thead>
+              <tr>
+                <th>Ação</th>
+                <th>Quantidade</th>
+                <th>Valor (R$)</th>
+                <th>Negociar</th>
+              </tr>
+            </thead>
 
-              <tbody>
-                {saleEquities.map((equity) => (
-                  <tr key={equity.CodAtivo}>
-                    <td>{equity.CodAtivo}</td>
-                    <td>{equity.QtdeAtivo}</td>
-                    <td>{equity.Valor}</td>
-                    <td>
-                      <button
-                          type="submit"
-                          onClick={(e) => handleClick(e, equity.CodAtivo)}
-                        >
-                          Comprar
-                        </button>
-                    </td>
-                  </tr>
-                ))}
-                </tbody>
-              </table>
-              :
-              <h2>Carregando...</h2>
+            <tbody>
+              {saleEquities.map((equity, index) => (
+                <tr key={equity[index]}>
+                  <td>{equity.CodAtivo}</td>
+                  <td>{equity.QtdeAtivo}</td>
+                  <td>{equity.Valor}</td>
+                  <td>
+                    <button
+                        type="submit"
+                        onClick={(e) => handleClick(e, equity.CodAtivo)}
+                      >
+                        Comprar
+                      </button>
+                  </td>
+                </tr>
+              ))}
+              </tbody>
+            </table>
+        : <h3>Não há ações disponíveis para compra.</h3>
       }
-
     </div>
   );
 }
