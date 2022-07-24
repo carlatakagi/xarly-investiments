1991b7999c5a3f29a77016cd7ea12029e4b6337e
carlatakagi <carlatakagi@gmail.com>
2022-07-24 16:07:00 -0400

    feat: changing file

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import EquitiesProvider from './context/EquitiesProvider';
-import Login from './pages/Login';
+import { EquitiesProvider } from './shared/providers/EquitiesProvider';
+import { UserProvider } from './shared/providers/UserProvider';
+import Login from './pages/Login/index';
 import Equities from './pages/Equities/index';
 import DepositAndWithdraw from './pages/DepositAndWithdraw/index';
 import PurchaseAndSale from './pages/PurchaseAndSale/index';
@@ -9,16 +10,19 @@ import PurchaseAndSale from './pages/PurchaseAndSale/index';
 function App() {
 
   return (
-    <EquitiesProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/*" element={<Login />} />
-          <Route path="/equities" element={<Equities />} />
-          <Route path="/depositandwithdraw" element={<DepositAndWithdraw />} />
-          <Route path="/purchaseandsale/:CodAtivo" element={<PurchaseAndSale />} />
-        </Routes>
-      </BrowserRouter>
-    </EquitiesProvider>
+    <UserProvider>
+      <EquitiesProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/*" element={<Login />} />
+            <Route path="/equities" element={<Equities />} />
+            <Route path="/depositandwithdraw" element={<DepositAndWithdraw />} />
+            <Route path="/purchaseandsale/:CodAtivo" element={<PurchaseAndSale />} />
+          </Routes>
+        </BrowserRouter>
+      </EquitiesProvider>
+    </UserProvider>
+
   )
 }
 
