112c2652730f98c60c90810250dc640b88ee6b8b
carlatakagi <carlatakagi@gmail.com>
2022-07-24 16:11:17 -0400

    feat: changing files

--- src/pages/Equities/index.jsx
+++ src/pages/Equities/index.jsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useEquitiesContext } from '../../shared/providers/EquitiesProvider';
+import { useUserContext } from '../../shared/providers/UserProvider';
 import Header from '../../components/Header/index';
 import TableMyEquities from '../../components/TableMyEquities';
 import TableSaleEquities from '../../components/TableSaleEquities';
@@ -7,6 +9,13 @@ import './styles.css';
 
 function Equities () {
   const navigate = useNavigate();
+  const { user } = useUserContext();
+  const { getEquitiesByClientId } = useEquitiesContext();
+
+  console.log(user);
+  useEffect(() => {
+    getEquitiesByClientId(user.clientId);
+  }, []);
 
   const handleClick = (e) => {
     e.preventDefault();
